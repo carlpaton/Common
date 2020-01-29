0ad40698c4224260411daec59c14eea801aa420a
Carl <carl.paton@gmail.com>
2020-01-29 19:49:43 +1300

    HttpWebRequestFactory to wrap static HTTP methods

---
+++ Common/Http/Wrapper/WrapHttpWebRequest.cs
@@ -0,0 +1,26 @@
+﻿using Common.Http.Interface;
+using System.Net;
+
+namespace Common.Http.Wrapper
+{
+    public class WrapHttpWebRequest : IHttpWebRequest
+    {
+        private readonly HttpWebRequest _request;
+
+        public WrapHttpWebRequest(HttpWebRequest request)
+        {
+            _request = request;
+        }
+
+        public string Method
+        {
+            get { return _request.Method; }
+            set { _request.Method = value; }
+        }
+
+        public IHttpWebResponse GetResponse()
+        {
+            return new WrapHttpWebResponse((HttpWebResponse)_request.GetResponse());
+        }
+    }
+}
